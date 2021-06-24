505dd7ce64de20683e28c458dea1e0dd51bb48c8
kilbasovich <vkilbasovich@gmail.com>
2021-06-24 09:59:30 +0300

    1 commit

--- tk_open_file.py
+++ tk_open_file.py
@@ -9,60 +9,61 @@ import select
 class App:
      def __init__(self,win):
           self.master = win
-          self.lbl1 = Label(win, text='П.І.Б. відвідувача')
-          self.lbl2 = Label(win, text='Хто дозволив')
-          self.lbl3 = Label(win, text='Телефон')
+          self.t1 = Entry(width=30)
+          self.t1.grid(column=1, row=1)
+          self.t2 = Entry(width=30)
+          self.t2.grid(column=1, row=2)
+          self.t3 = Entry(width=30)
+          self.t3.grid(column=1, row=3)
 
-          self.t1 = Entry(bd=3)
-          self.t2 = Entry()
-          self.t3 = Entry()
-          self.show_db = Listbox(width=50)
-          
-
-
-          self.lbl1.place(x=100, y=50)
-          self.lbl2.place(x=100, y=100)
-          self.lbl3.place(x=100, y=150)
-          self.show_db.place(x=100, y=350)
-
-          
-          
-
-          self.t1.place(x=200, y=50)
-          self.t2.place(x=200, y=100)
-          self.t3.place(x=200, y=150)
+          self.lbl1 = Label(win, text='П.І.Б. відвідувача', justify=LEFT)
+          self.lbl1.grid(column=0, row=1)
+          self.lbl2 = Label(win, text='Хто дозволив', justify=LEFT)
+          self.lbl2.grid(column=0, row=2)
+          self.lbl3 = Label(win, text='Телефон', justify=LEFT)
+          self.lbl3.grid(column=0, row=3)
 
           self.b1 = Button(win, text='Зберегти', command=self.add)
+          self.b1.grid(column=0, row=4)
           self.b2 = Button(win, text='Друк', command=self.printer)
+          self.b2.grid(column=1, row=4)
           self.b3 = Button(win, text='завантажити базу', command=self.select_dbase)
-
-          self.b1.place(x=100, y=250)
-          self.b2.place(x=200, y=250)
-          self.b3.place(x=300, y=250)
-          
-          
+          self.b3.grid(column=1, row=5)
+              
 
      def add(self):
           name1 = self.t1.get()
           name2 = self.t2.get()
-          tel = int(self.t3.get())
+          tel = self.t3.get()
           data_time = datetime.datetime.today().strftime('%Y-%m-%d %X')
-          insert_into.ins_in([name1, name2, tel, data_time])
+          insert_into.ins_in([name1, name2, int(tel), data_time])
+          self.t1.delete(0,END)
+          self.t2.delete(0,END)
+          self.t3.delete(0,END)
 
      def printer(self):
-          os.startfile(r'C:\Users\admin\Desktop\python\проходная_пропуск\12.txt', 'print')
+          s = self.show_db.curselection()
+          s2 = self.show_db.get(s[0])
+          print(s2)
+          strig = 'Відвідувач: '+ str(s2[1])+'\n' +'Працівник: '+ str(s2[2])+'\n' +  'телефон: '+ str(s2[3])+'\n'+ str(s2[4])
+          f = open(r'12.txt', 'w')
+          f.write(strig)
+          f.close
+          os.startfile(r'12.txt', 'print')
+          
 
      def select_dbase(self):
-          i = 0
+          self.show_db = Listbox(width=50, selectbackground='gray')
+          self.show_db.grid(column=1, row=7)
           insert_db_text = select.sel()
           for i in insert_db_text:
-               self.show_db.insert(0, i)  
-          
+               self.show_db.insert(END, i)
+
+              
+                
 
 window = Tk()
 app = App(window)
-window.title('Перепустка на Python')
-window.geometry("500x600+10+10")
+window.title('Перепустка')
+window.geometry("500x500+10+10")
 window.mainloop()
-          
-input()
